f94b4bbf01ba10ddd86c0c714178575a0e66c65c
AlefeVariani <alefevariani@unochapeco.edu.br>
2014-06-21 23:18:30 -0300

    Primeiro Commit da Applicação

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
+
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
